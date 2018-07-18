b18a37bc802c17a2bb6dc710bfcfce0934026c6f
Paul Rossotti <rossotti.paul@gmail.com>
2018-07-18 09:06:45 -0700

    added testMake_bool_point_diff_mat

--- teamBoxScore/testLeadingInTheFirstHalfWinning.py
+++ teamBoxScore/testLeadingInTheFirstHalfWinning.py
@@ -14,3 +14,15 @@ class MyTest(unittest.TestCase):
     def testMake_point_diff_mat(self):
         x = LeadingInTheFirstHalfWinning.make_point_diff_mat(self.df)
         self.assertEqual(x.shape, (2, 2))
+        self.assertEqual(x[0, 0], 10)
+        self.assertEqual(x[0, 1], 5)
+        self.assertEqual(x[1, 0], 2)
+        self.assertEqual(x[1, 1], -3)
+
+    def testMake_bool_point_diff_mat(self):
+        x = LeadingInTheFirstHalfWinning.make_bool_point_diff_mat(self.df)
+        self.assertEqual(x.shape, (2, 2))
+        self.assertEqual(x[0, 0], 1)
+        self.assertEqual(x[0, 1], 1)
+        self.assertEqual(x[1, 0], 1)
+        self.assertEqual(x[1, 1], -1)
