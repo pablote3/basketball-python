56484da9c99b0f1f2d7f5c06b366eea41001dd1b
Paul Rossotti <rossotti.paul@gmail.com>
2018-07-18 11:05:20 -0700

    account for ties

--- teamBoxScore/LeadingInTheFirstHalfWinning.py
+++ teamBoxScore/LeadingInTheFirstHalfWinning.py
@@ -1,29 +1,25 @@
 import numpy as np
-from numpy.polynomial.polynomial import polyfit
 import pandas as pd
 import matplotlib.pyplot as plt
-import unittest
 
 pd.options.mode.chained_assignment = None
-DOWN_AT_HALF = -1
-TIE_AT_HALF = 0
-UP_AT_HALF = 1
+DOWN = -1
+TIE = 0
+UP = 1
 
 df = pd.read_csv("../input/2017-18_teamBoxScore.csv")
 
 df2 = df[["teamAbbr", "teamPTS", "teamPTS1", "teamPTS2", "opptPTS", "opptPTS1", "opptPTS2"]]
 
-df2.loc[:, "teamPTSH1"] = df2["teamPTS1"] + df["teamPTS2"]
-df2.loc[:, "opptPTSH1"] = df2["opptPTS1"] + df["opptPTS2"]
+df2.loc[:, "teamPTSHalf"] = df2["teamPTS1"] + df2["teamPTS2"]
+df2.loc[:, "opptPTSHalf"] = df2["opptPTS1"] + df2["opptPTS2"]
 
-df2.loc[:, "ptdiffH1"] = df2["teamPTSH1"] - df2["opptPTSH1"]
-df2.loc[:, "ptdiff"] = df2["teamPTS"] - df2["opptPTS"]
-df = df[["teamAbbr", "teamLoc", "teamPTS", "teamPTS1", "teamPTS2", "teamPTS3", "teamPTS4", "teamPTS5",
-         "opptAbbr", "opptLoc", "opptPTS", "opptPTS1", "opptPTS2", "opptPTS3", "opptPTS4", "opptPTS5"]]
+df2.loc[:, "ptDiffHalf"] = df2["teamPTSHalf"] - df2["opptPTSHalf"]
+df2.loc[:, "ptDiffFull"] = df2["teamPTS"] - df2["opptPTS"]
 
 
 def make_point_diff_mat(df):
-    point_diff_df = df[["ptdiffH1", "ptdiff"]]
+    point_diff_df = df[["ptDiffHalf", "ptDiffFull"]]
     point_diff = point_diff_df.as_matrix()
     return point_diff
 
@@ -31,14 +27,15 @@ def make_point_diff_mat(df):
 def make_bool_point_diff_mat(df):
     point_diff = make_point_diff_mat(df)
     bool_point_diff = np.copy(point_diff)
-    bool_point_diff[bool_point_diff > 0] = 1
-    bool_point_diff[bool_point_diff < 0] = -1
+    bool_point_diff[bool_point_diff > 0] = UP
+    bool_point_diff[bool_point_diff == 0] = TIE
+    bool_point_diff[bool_point_diff < 0] = DOWN
     return bool_point_diff
 
 
 def prob_of_winning_given(bool_point_diff, event):
     return np.mean((bool_point_diff[bool_point_diff[:, 0] == event][:, 1] + 1) / 2)
 
-#point_diff = make_point_diff_mat(df2)
+# point_diff = make_point_diff_mat(df2)
 # np.corrcoef(point_diff.T)
 # print(np)
--- teamBoxScore/testLeadingInTheFirstHalfWinning.py
+++ teamBoxScore/testLeadingInTheFirstHalfWinning.py
@@ -6,23 +6,28 @@ import pandas as pd
 class MyTest(unittest.TestCase):
     def setUp(self):
         self.data = [
-                        {"ptdiffH1": 10, "ptdiff": 5},
-                        {"ptdiffH1": 2, "ptdiff": -3}
+                        {"ptDiffHalf": -4, "ptDiffFull": 5},
+                        {"ptDiffHalf": 0, "ptDiffFull": 2},
+                        {"ptDiffHalf": 2, "ptDiffFull": -3}
                     ]
         self.df = pd.DataFrame(self.data)
 
     def testMake_point_diff_mat(self):
         x = LeadingInTheFirstHalfWinning.make_point_diff_mat(self.df)
-        self.assertEqual(x.shape, (2, 2))
-        self.assertEqual(x[0, 0], 10)
+        self.assertEqual(x.shape, (3, 2))
+        self.assertEqual(x[0, 0], -4)
         self.assertEqual(x[0, 1], 5)
-        self.assertEqual(x[1, 0], 2)
-        self.assertEqual(x[1, 1], -3)
+        self.assertEqual(x[1, 0], 0)
+        self.assertEqual(x[1, 1], 2)
+        self.assertEqual(x[2, 0], 2)
+        self.assertEqual(x[2, 1], -3)
 
     def testMake_bool_point_diff_mat(self):
         x = LeadingInTheFirstHalfWinning.make_bool_point_diff_mat(self.df)
-        self.assertEqual(x.shape, (2, 2))
-        self.assertEqual(x[0, 0], 1)
+        self.assertEqual(x.shape, (3, 2))
+        self.assertEqual(x[0, 0], -1)
         self.assertEqual(x[0, 1], 1)
-        self.assertEqual(x[1, 0], 1)
-        self.assertEqual(x[1, 1], -1)
+        self.assertEqual(x[1, 0], 0)
+        self.assertEqual(x[1, 1], 1)
+        self.assertEqual(x[2, 0], 1)
+        self.assertEqual(x[2, 1], -1)
