342e71ec3b945626c0addd73734826d84609f8d1
Paul Rossotti <rossotti.paul@gmail.com>
2018-07-18 20:35:56 -0700

    add best fit line

--- teamBoxScore/LeadingInTheFirstHalfWinning.py
+++ teamBoxScore/LeadingInTheFirstHalfWinning.py
@@ -1,4 +1,5 @@
 import numpy as np
+from numpy.polynomial.polynomial import polyfit
 import pandas as pd
 import matplotlib.pyplot as plt
 
@@ -13,14 +14,14 @@ df2 = df[["teamAbbr", "teamPTS", "teamPTS1", "teamPTS2", "opptPTS", "opptPTS1", 
 
 df2.loc[:, "teamPTSHalf"] = df2["teamPTS1"] + df2["teamPTS2"]
 df2.loc[:, "opptPTSHalf"] = df2["opptPTS1"] + df2["opptPTS2"]
-
 df2.loc[:, "ptDiffHalf"] = df2["teamPTSHalf"] - df2["opptPTSHalf"]
+
 df2.loc[:, "ptDiffFull"] = df2["teamPTS"] - df2["opptPTS"]
 
 
 def make_point_diff_mat(df):
     point_diff_df = df[["ptDiffHalf", "ptDiffFull"]]
-    point_diff = point_diff_df.as_matrix()
+    point_diff = point_diff_df.values
     return point_diff
 
 
@@ -36,6 +37,21 @@ def make_bool_point_diff_mat(df):
 def prob_of_winning_given(bool_point_diff, event):
     return np.mean((bool_point_diff[bool_point_diff[:, 0] == event][:, 1] + 1) / 2)
 
-# point_diff = make_point_diff_mat(df2)
-# np.corrcoef(point_diff.T)
-# print(np)
+
+point_diff = make_point_diff_mat(df2)
+print(np.corrcoef(point_diff.T))
+
+x = point_diff[:, 0]
+y = point_diff[:, 1]
+b, m = polyfit(x, y, 1)
+plt.plot(x, y, '.')
+plt.plot(x, b + m * x, '-')
+
+plt.scatter(x, y)
+plt.ylabel("point differential: end of game")
+plt.xlabel("point differential: end of first half")
+plt.show()
+
+bool_point_diff = make_bool_point_diff_mat(df2)
+print(np.corrcoef(bool_point_diff.T))
+prob_of_winning_given(bool_point_diff, DOWN)
