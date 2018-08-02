b8884a33a0ed337b09d3033a49deefd873f548dd
Paul Rossotti <rossotti.paul@gmail.com>
2018-08-01 18:13:49 -0700

    additional test cases

--- toolbox/DataTypes_Numeric.py
+++ toolbox/DataTypes_Numeric.py
@@ -9,25 +9,66 @@ class DataTypesNumeric(unittest.TestCase):
         cls.flt1 = 10.1
         cls.flt2 = 2.00
 
-        cls.listNumber = [1, 2, 3, 4, 5]
-        cls.tuple = ('Geeks', 'for', 'geeks')
-
     def test_instanceInt(self):
-        self.assertTrue(type(self.int1), int)
-        self.assertTrue(type(self.int1), float)
-        self.assertTrue(type(self.int1), str)
-        self.assertTrue(type(self.int1), bool)
+        self.assertTrue(isinstance(self.int1, int))
+        self.assertTrue(int, type(self.int1))
+        self.assertTrue(int, isinstance(self.int1, float))
+        self.assertTrue(float, type(self.int1))
+        self.assertTrue(str, type(self.int1))
+        self.assertTrue(bool, type(self.int1))
 
     def test_instanceFloat(self):
-        self.assertTrue(type(self.flt1), int)
-        self.assertTrue(type(self.flt1), float)
-        self.assertTrue(type(self.flt1), str)
-        self.assertTrue(type(self.flt1), bool)
+        self.assertFalse(isinstance(self.flt1, int))
+        self.assertTrue(int, type(self.flt1))
+        self.assertTrue(isinstance(self.flt1, float))
+        self.assertTrue(float, type(self.flt1))
+        self.assertTrue(str, type(self.flt1))
+        self.assertTrue(bool, type(self.flt1))
 
     def test_add(self):
-        self.assertEqual(self.int1 + self.int2, 12)
-        self.assertEqual(self.flt1 + self.flt2, 12.1)
-        self.assertEqual(self.int1 + self.flt1, 20.1)
+        self.assertEqual(12, self.int1 + self.int2)
+        self.assertEqual(12.1, self.flt1 + self.flt2)
+        self.assertEqual(20.1, self.int1 + self.flt1)
+
+    def test_subtract(self):
+        self.assertEqual(8, self.int1 - self.int2)
+        self.assertEqual(8.1, self.flt1 - self.flt2)
+        self.assertEqual(-0.09999999999999964, self.int1 - self.flt1)
+
+    def test_multiply(self):
+        self.assertEqual(20, self.int1 * self.int2)
+        self.assertEqual(20.2, self.flt1 * self.flt2)
+        self.assertEqual(101.0, self.int1 * self.flt1)
+
+    def test_divide(self):
+        self.assertEqual(5, self.int1 / self.int2)
+        self.assertEqual(5.05, self.flt1 / self.flt2)
+        self.assertEqual(0.9900990099009901, self.int1 / self.flt1)
+
+    def test_floorDivide(self):
+        self.assertEqual(5, self.int1 // self.int2)
+        self.assertEqual(5, self.flt1 // self.flt2)
+        self.assertEqual(0, self.int1 // self.flt1)
+
+    def test_exponent(self):
+        self.assertEqual(100, self.int1 ** self.int2)
+        self.assertEqual(102.00999999999999, self.flt1 ** self.flt2)
+        self.assertEqual(100.0, self.int1 ** self.flt2)
+
+    def test_equals(self):
+        self.assertTrue(self.int1 != self.int2)
+        self.assertTrue(self.int1 == self.int1)
+        self.assertTrue(self.int2 == self.flt2)
+
+    def test_greaterThan(self):
+        self.assertTrue(self.int1 >= self.int2)
+        self.assertTrue(self.int1 >= self.int1)
+        self.assertTrue(self.int2 >= self.flt2)
+
+    def test_is(self):
+        self.assertFalse(self.int1 is self.int2)
+        self.assertTrue(self.int1 is self.int1)
+        self.assertTrue(self.int2 is not self.flt2)
 
 
 if __name__ == '__main__':
