6dd2414fb3a1b848d0f595b83cf8222f085fa0ca
Paul Rossotti <rossotti.paul@gmail.com>
2018-08-07 07:34:00 -0700

    rename file

--- toolbox/PythonConditionals.py
+++ toolbox/PythonConditionals.py
@@ -3,16 +3,16 @@ import unittest
 
 class PythonConditions(unittest.TestCase):
     def test_if(self):
-        self.assertEqual("Greater", condition(34, 33))
-        self.assertEqual("Equal", condition(34, 34))
-        self.assertEqual("Less", condition(34, 35))
+        self.assertEqual("Greater", conditionIf(34, 33))
+        self.assertEqual("Equal", conditionIf(34, 34))
+        self.assertEqual("Less", conditionIf(34, 35))
 
 
 if __name__ == '__main__':
     unittest.main()
 
 
-def condition(int1, int2):
+def conditionIf(int1, int2):
     if int1 > int2:
         return "Greater"
     elif int1 < int2:
