da3f1523554589864fc4941280ea5093e244a0c8
Paul Rossotti <rossotti.paul@gmail.com>
2018-08-17 20:40:33 -0700

    expand function_if

--- toolbox/Functions.py
+++ toolbox/Functions.py
@@ -5,7 +5,7 @@ class Functions(unittest.TestCase):
     def test_if(self):
         self.assertEqual("Greater", function_if(34, 33))
         self.assertEqual("Greater", function_if(34))
-        self.assertEqual("Equal", function_if(34, 34))
+        self.assertEqual(None, function_if(34, 34))
         self.assertEqual("Less", function_if(34, 35))
 
 
@@ -13,10 +13,8 @@ if __name__ == '__main__':
     unittest.main()
 
 
-def function_if(int1, int2=1):
-    if int1 > int2:
+def function_if(positional1, keyword2=1):
+    if positional1 > keyword2:
         return "Greater"
-    elif int1 < int2:
+    elif positional1 < keyword2:
         return "Less"
-    else:
-        return "Equal"
