ea5e9c39edb9c0f7ca743e7bc03fdf81453ba0ca
Paul Rossotti <rossotti.paul@gmail.com>
2018-08-19 18:43:50 -0700

    added function clean strings

--- toolbox/Functions.py
+++ toolbox/Functions.py
@@ -1,4 +1,5 @@
 import unittest
+import re
 
 
 class Functions(unittest.TestCase):
@@ -11,6 +12,13 @@ class Functions(unittest.TestCase):
     def test_multiple_return_values(self):
         self.assertEqual((5, 6, 7), function_multiple_return_values_tuple())
         self.assertEqual({'a': 5, 'b': 6, 'c': 7}, function_multiple_return_values_dict())
+
+    def test_regular_expressions(self):
+        self.assertEqual('Alabama', function_str_string('  Alabama  '))
+        self.assertEqual('Georgia', function_remove_punctuation('?Georgia!'))
+        self.assertEqual('Georgia', function_str_title('georgia'))
+        self.assertEqual('Florida', function_str_title('FlOrIda'))
+        self.assertEqual('South  Carolina', function_clean_strings('!south  carolina###'))
 
 
 if __name__ == '__main__':
@@ -36,3 +44,22 @@ def function_multiple_return_values_dict():
     b = 6
     c = 7
     return {'a': a, 'b': b, 'c': c}
+
+
+def function_remove_punctuation(value):
+    return re.sub('[!#?]', '', value)   #remove special characters
+
+
+def function_str_string(value):
+    return str.strip(value)             #remove whitespace
+
+
+def function_str_title(value):
+    return str.title(value)             #camel case
+
+
+def function_clean_strings(value):
+    value = function_str_string(value)
+    value = function_remove_punctuation(value)
+    value = function_str_title(value)
+    return value
