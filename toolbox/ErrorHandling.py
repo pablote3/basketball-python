7c216688b18570227f95aa4f624b77579e356faa
Paul Rossotti <rossotti.paul@gmail.com>
2018-08-21 06:05:29 -0700

    added open file tests

--- toolbox/ErrorHandling.py
+++ toolbox/ErrorHandling.py
@@ -4,17 +4,41 @@ import unittest
 class Functions(unittest.TestCase):
 
     def test_exception_handling(self):
-        self.assertEqual(12.25, function_exception_handling(12.25))
-        self.assertEqual('three', function_exception_handling('three'))
+        self.assertEqual(12.25, function_catch_except(12.25))
+        self.assertEqual('exception', function_catch_except('three'))
+        self.assertEqual('success', function_catch_value_error(12.25))
+        self.assertEqual('valueError', function_catch_value_error('three'))
+        self.assertEqual('openSuccess', function_open_file('/home/pablote/Documents/python/notes/paul.txt'))
+        self.assertEqual('openFailed', function_open_file('/home/pablote/Documents/python/notes/paul.bat'))
 
 
 if __name__ == '__main__':
     unittest.main()
 
 
-def function_exception_handling(n=10):
+def function_catch_except(n=10):
     try:
         return float(n)
-    
     except:
-        return n
+        return 'exception'
+
+
+def function_catch_value_error(n=10):
+    try:
+        float(n)
+    except ValueError:
+        return 'valueError'
+    else:
+        return 'success'
+
+
+def function_open_file(path):
+    try:
+        f = open(path, 'r')
+    except:
+        return 'openFailed'
+    else:
+        f.close()
+        return 'openSuccess'
+    # finally:
+    #     f.close()                 #UnboundLocalError if f not loaded
