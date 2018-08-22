a5ef551e8bef5e523a974096f06d0aac58614d8b
Paul Rossotti <rossotti.paul@gmail.com>
2018-08-22 06:23:19 -0700

    added read file tests

--- toolbox/ErrorHandling.py
+++ toolbox/ErrorHandling.py
@@ -5,16 +5,18 @@ from FileHandling import FileHandling
 
 class ErrorHandling(unittest.TestCase):
     def test_exception_handling(self):
-        absolute_path = "/home/pablote/Documents/python/notes/"
-        relative_path = os.path.dirname(__file__) + "/"                 #absolute dir the script is in
+        absolute_path = "/home/pablote/git/basketball-python/toolbox"
+        relative_path = os.path.dirname(__file__)                        #absolute dir the script is in
         self.assertEqual(12.25, function_catch_except(12.25))
         self.assertEqual('exception', function_catch_except('three'))
         self.assertEqual('success', function_catch_value_error(12.25))
         self.assertEqual('valueError', function_catch_value_error('three'))
-        self.assertEqual(2, FileHandling.function_open_file_read(absolute_path + 'paulRead.txt'))
-        self.assertEqual('openFailed', FileHandling.function_open_file_read(absolute_path + 'paulo.txt'))
-        self.assertEqual(2, FileHandling.function_open_file_read(relative_path + 'paulRead.txt'))
-        self.assertEqual('openFailed', FileHandling.function_open_file_read(relative_path + 'paulo.txt'))
+        self.assertEqual(2, FileHandling.function_read_line(absolute_path + '/paulRead.txt'))
+        self.assertEqual('openFailed', FileHandling.function_read_line(absolute_path + '/paulo.txt'))
+        self.assertEqual(2, FileHandling.function_read_line(relative_path + '/paulRead.txt'))
+        self.assertEqual('openFailed', FileHandling.function_read_line(relative_path + '/paulo.txt'))
+        self.assertEqual('line1\nline2', FileHandling.function_read_file(relative_path + '/paulRead.txt'))
+        self.assertEqual('openFailed', FileHandling.function_read_file(relative_path + '/paulo.txt'))
 
 
 if __name__ == '__main__':
--- toolbox/FileHandling.py
+++ toolbox/FileHandling.py
@@ -1,10 +1,29 @@
 class FileHandling:
-    def function_open_file_read(self):
+
+    def function_read_line(self):
         try:
             with open(self, 'r') as f:              #auto file close when block exits
                 lines = [x.rstrip() for x in f]
             return lines.__len__()
         except:
             return 'openFailed'
+
+    def function_read_file(self):
+        try:
+            f = open(self, 'r')
+            return f.read()
+        except:
+            return 'openFailed'
+        finally:
+            if 'f' in locals():
+                f.close()
+
+    def function_open_file_write(self):
+        try:
+            with open(self, 'w') as f:
+                lines = [x.rstrip() for x in f]     #auto file close when block exits
+        except:
+            return 'openFailed'
         else:
             f.close()
+            return 'openSuccess'
