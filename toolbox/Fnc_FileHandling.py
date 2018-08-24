3fd00e401abcee4dc6d167553089ca1cc27c7bb0
Paul Rossotti <rossotti.paul@gmail.com>
2018-08-23 17:23:39 -0700

    added create file tests

--- toolbox/Fnc_FileHandling.py
+++ toolbox/Fnc_FileHandling.py
@@ -3,51 +3,59 @@ import os
 
 class FncFileHandling:
 
+    def create_file(self):
+        if not os.path.exists(self):
+            f = open(self, "w")
+            f.close()
+            return "createSuccess"
+        else:
+            return "fileExists"
+
     def delete_file(self):
         if os.path.exists(self):
             os.remove(self)
-            return 'deleteSuccess'
+            return "deleteSuccess"
         else:
-            return 'fileNotFound'
+            return "fileNotFound"
 
     def read_line(self):
         try:
-            with open(self, 'r') as f:              #auto file close when block exits
+            with open(self, "r") as f:              #auto file close when block exits
                 lines = [x.rstrip() for x in f]
             return lines.__len__()
         except:
-            return 'openFailed'
+            return "openFailed"
 
     def read_string(self, char):
         try:
-            f = open(self, 'r')
+            f = open(self, "r")
             if char > 0:
                 return f.read(char)
             else:
                 return f.read()
         except:
-            return 'openFailed'
+            return "openFailed"
         finally:
-            if 'f' in locals():
+            if "f" in locals():
                 f.close()
 
     def append_line(self, value):
         try:
-            f = open(self, 'a')
+            f = open(self, "a")
             f.write(value)
             f.close()
-            with open(self, 'r') as f:
+            with open(self, "r") as f:
                 lines = [x.rstrip() for x in f]
             return lines.__len__()
         except:
             return "openFailed"
         finally:
-            if 'f' in locals():
+            if "f" in locals():
                 f.close()
 
     def remove_line(self, value):
         try:
-            f = open(self, 'r+')
+            f = open(self, "r+")
             d = f.readlines()
             f.seek(0)
             for i in d:
@@ -57,15 +65,15 @@ class FncFileHandling:
         except:
             return "openFailed"
         finally:
-            if 'f' in locals():
+            if "f" in locals():
                 f.close()
 
     def open_file_write(self):
         try:
-            with open(self, 'w') as f:
+            with open(self, "w") as f:
                 lines = [x.rstrip() for x in f]     #auto file close when block exits
         except:
-            return 'openFailed'
+            return "openFailed"
         else:
             f.close()
-            return 'openSuccess'
+            return "openSuccess"
--- toolbox/TestFileHandling.py
+++ toolbox/TestFileHandling.py
@@ -10,6 +10,10 @@ class TestFileHandling(unittest.TestCase):
         cls.rel_path = os.path.dirname(__file__) + "/files"                     #relative working directory
         FncFileHandling.append_line(cls.abs_path + "/paulCreate.txt", "\nAppended")
 
+    def test_create_file(self):
+        self.assertEqual("createSuccess", FncFileHandling.create_file(self.abs_path + "/paulMade.txt"))
+        self.assertEqual("fileExists", FncFileHandling.create_file(self.abs_path + "/paulRead.txt"))
+
     def test_read_file(self):
         self.assertEqual(2, FncFileHandling.read_line(self.abs_path + "/paulRead.txt"))
         self.assertEqual(2, FncFileHandling.read_line(self.rel_path + "/paulRead.txt"))
@@ -18,15 +22,20 @@ class TestFileHandling(unittest.TestCase):
         self.assertEqual("line", FncFileHandling.read_string(self.rel_path + "/paulRead.txt", 4))
         self.assertEqual("openFailed", FncFileHandling.read_string(self.rel_path + "/paulo.txt", 0))
 
-    def test_append_write(self):
+    def test_append_file(self):
         self.assertEqual(3, FncFileHandling.append_line(self.abs_path + "/paulAppend.txt", "Appended"))    #exists
         self.assertEqual(1, FncFileHandling.append_line(self.abs_path + "/paulAppender.txt", "Appended"))  #not exist
 
+    # def test_write_file(self):
+    #     self.assertEqual(3, FncFileHandling.append_line(self.abs_path + "/paulAppend.txt", "Appended"))
+    #     self.assertEqual(1, FncFileHandling.append_line(self.abs_path + "/paulAppender.txt", "Appended"))
+
     def test_delete_file(self):
-        self.assertEqual("deleteSuccess", FncFileHandling.delete_file(self.abs_path + "/paulCreate.txt"))  #exists
-        self.assertEqual("fileNotFound", FncFileHandling.delete_file(self.abs_path + "/paulFish.txt"))     #not exist
+        self.assertEqual("deleteSuccess", FncFileHandling.delete_file(self.abs_path + "/paulCreate.txt"))
+        self.assertEqual("fileNotFound", FncFileHandling.delete_file(self.abs_path + "/paulFish.txt"))
 
     @classmethod
     def tearDownClass(cls):
         FncFileHandling.remove_line(cls.abs_path + "/paulAppend.txt", "Appended")
         FncFileHandling.delete_file(cls.abs_path + "/paulAppender.txt")
+        FncFileHandling.delete_file(cls.abs_path + "/paulMade.txt")
