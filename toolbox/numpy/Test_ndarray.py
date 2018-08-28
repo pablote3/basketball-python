fa3ac829359111d6018b4a755897e62144e1945f
Paul Rossotti <rossotti.paul@gmail.com>
2018-08-27 18:05:40 -0700

    added test create two dimensional array

--- toolbox/numpy/Test_ndarray.py
+++ toolbox/numpy/Test_ndarray.py
@@ -3,12 +3,22 @@ import numpy as np
 
 
 class TestNumpyArray(unittest.TestCase):
-    def test_array_create_float(self):
-        list1 = [6, 7.5, 8, 0, 1]
-        arr1 = np.array(list1)
+    def test_create_float_array(self):
+        arr1 = np.array([6, 7.5, 8, 0, 1])
         self.assertEqual("<class 'numpy.ndarray'>", str(type(arr1)))
+        self.assertEqual("float64", arr1.dtype)             #data type of array
+        self.assertEqual(1, arr1.ndim)                      #number of dimensions
         self.assertEqual((5,), arr1.shape)
         self.assertEqual(6., arr1[0])
+
+    def test_create_two_dimension_array(self):
+        arr1 = np.array([[1, 2, 3], [4, 5, 6]])
+        self.assertEqual("<class 'numpy.ndarray'>", str(type(arr1)))
+        self.assertEqual("int64", arr1.dtype)             #data type of array
+        self.assertEqual(2, arr1.ndim)                      #number of dimensions
+        self.assertEqual((2, 3), arr1.shape)
+        self.assertEqual(1, arr1[0, 0])
+        self.assertEqual(4, arr1[1, 0])
 
 
 if __name__ == '__main__':
