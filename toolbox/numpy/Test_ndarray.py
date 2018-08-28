5d25fe3c9826f46321c6c6b1175b3fbc6100bbba
Paul Rossotti <rossotti.paul@gmail.com>
2018-08-27 17:41:51 -0700

    added test array create floats

--- toolbox/numpy/Test_ndarray.py
+++ toolbox/numpy/Test_ndarray.py
@@ -3,10 +3,12 @@ import numpy as np
 
 
 class TestNumpyArray(unittest.TestCase):
-    def test_array_create(self):
+    def test_array_create_float(self):
         list1 = [6, 7.5, 8, 0, 1]
         arr1 = np.array(list1)
         self.assertEqual("<class 'numpy.ndarray'>", str(type(arr1)))
+        self.assertEqual((5,), arr1.shape)
+        self.assertEqual(6., arr1[0])
 
 
 if __name__ == '__main__':
