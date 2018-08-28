77096f09d379d171441bf44445c6724fdf3bccc2
Paul Rossotti <rossotti.paul@gmail.com>
2018-08-28 05:54:10 -0700

    additional data conversion tests

--- toolbox/numpy/Test_ndarray.py
+++ toolbox/numpy/Test_ndarray.py
@@ -6,16 +6,16 @@ class TestNumpyArray(unittest.TestCase):
     def test_create_float_array(self):
         arr1 = np.array([6, 7.5, 8, 0, 1])
         self.assertEqual("<class 'numpy.ndarray'>", str(type(arr1)))
-        self.assertEqual("float64", arr1.dtype)             #data type of array
-        self.assertEqual(1, arr1.ndim)                      #number of dimensions
+        self.assertEqual("float64", arr1.dtype)                     #data type of array
+        self.assertEqual(1, arr1.ndim)                              #number of dimensions
         self.assertEqual((5,), arr1.shape)
         self.assertEqual(6., arr1[0])
 
     def test_create_two_dimension_array(self):
         arr1 = np.array([[1, 2, 3], [4, 5, 6]])
         self.assertEqual("<class 'numpy.ndarray'>", str(type(arr1)))
-        self.assertEqual("int64", arr1.dtype)               #data type of array
-        self.assertEqual(2, arr1.ndim)                      #number of dimensions
+        self.assertEqual("int64", arr1.dtype)                       #data type of array
+        self.assertEqual(2, arr1.ndim)                              #number of dimensions
         self.assertEqual((2, 3), arr1.shape)
         self.assertEqual(1, arr1[0, 0])
         self.assertEqual(4, arr1[1, 0])
@@ -33,22 +33,35 @@ class TestNumpyArray(unittest.TestCase):
         self.assertEqual(1, arr1[0, 1])
 
     def test_create_empty_array(self):
-        arr1 = np.empty((2, 3, 2))                        #not initialized
+        arr1 = np.empty((2, 3, 2))                                  #not initialized
         self.assertEqual(3, arr1.ndim)
         self.assertEqual((2, 3, 2), arr1.shape)
         self.assertIsNotNone(arr1[0, 1, 0])
 
     def test_create_arrange_array(self):
-        arr1 = np.arange(15)                            #built in range function
+        arr1 = np.arange(15)                                        #built in range function
         self.assertEqual(1, arr1.ndim)
         self.assertEqual((15, ), arr1.shape)
         self.assertEqual(0, arr1[0])
 
     def test_convert_dtype_array(self):
-        arr1 = np.array([1, 2, 3, 4, 5])
+        arr1 = np.array([1, 2, 3, 4, 5])                            #int64 to float64
         self.assertEqual('int64', arr1.dtype)
         arr2 = arr1.astype(np.float64)
         self.assertEqual('float64', arr2.dtype)
+        self.assertEqual(1.0, arr2[0, ])
+
+        arr1 = np.array([1.0, 2.0, 3.0, 4.0, 5.0])                  #float64 to int32
+        self.assertEqual('float64', arr1.dtype)
+        arr2 = arr1.astype(np.int32)
+        self.assertEqual('int32', arr2.dtype)
+        self.assertEqual(1, arr2[0, ])
+
+        arr1 = np.array(['1.25', '-9.6', '42'], dtype=np.string_)   #string to float64
+        self.assertEqual('|S4', arr1.dtype)
+        arr2 = arr1.astype(np.float)
+        self.assertEqual('float', arr2.dtype)
+        self.assertEqual(1.25, arr2[0, ])
 
 
 if __name__ == '__main__':
