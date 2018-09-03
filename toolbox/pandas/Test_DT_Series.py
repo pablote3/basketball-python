113a7aac5263b90480b522041e176d689d6a19e0
Paul Rossotti <rossotti.paul@gmail.com>
2018-09-03 15:51:10 -0700

    added tests for create array

--- toolbox/pandas/Test_DT_Series.py
+++ toolbox/pandas/Test_DT_Series.py
@@ -4,9 +4,15 @@ import pandas as pd
 
 class TestNumpyArray(unittest.TestCase):
     def test_create_array(self):
-        arr1 = pd.Series([4, 7, -5, 3])
-        self.assertTrue(([4, 7, -5, 3] == arr1.values).all())           #array representation
-        self.assertTrue(([0, 1, 2, 3] == arr1.index.values).all())      #index
+        arr1 = pd.Series([4, 7, -5, 3])                                 #default index
+        self.assertTrue(([4, 7, -5, 3] == arr1.values).all())
+        self.assertTrue((pd.Index([0, 1, 2, 3] == arr1.index)).all())
+        self.assertTrue((pd.RangeIndex(start=0, stop=4, step=1) == arr1.index).all())
+
+        arr1 = pd.Series([4, 7, -5, 3], index=['d', 'b', 'a', 'c'])     #index with label
+        self.assertTrue((pd.Index(['d', 'b', 'a', 'c'] == arr1.index)).all())
+        self.assertEqual(7, arr1['b'])
+        self.assertTrue(([3, -5, 4] == arr1[['c', 'a', 'd']]).all())
 
 
 if __name__ == '__main__':
