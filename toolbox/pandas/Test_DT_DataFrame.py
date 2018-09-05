a7641f6b32bca8958ce7b99273af57ed829af12c
Paul Rossotti <rossotti.paul@gmail.com>
2018-09-04 22:07:07 -0700

    retrieve from dataframe tests

--- toolbox/pandas/Test_DT_DataFrame.py
+++ toolbox/pandas/Test_DT_DataFrame.py
@@ -19,6 +19,16 @@ class TestPandasDataFrame(unittest.TestCase):
         self.assertTrue((pd.isnull(frame['debt'])).all())                       #retrieve column as series as dict
         self.assertTrue((pd.notnull(frame.year)).all())                         #retrieve column as series by attribute
 
+        series1 = pd.Series(['Ohio', 'Ohio', 'Ohio', 'Nevada', 'Nevada', 'Nevada'],
+                            index=['one', 'two', 'three', 'four', 'five', 'six'])
+        self.assertTrue((series1 == frame['state']).all())                      #retrieve column as a series
+        self.assertEqual(6, frame['state'].size)
+
+        result = pd.Index(['year', 'state', 'pop', 'debt'])
+        self.assertTrue((result == frame.loc['three'].index).all())             #retrieve by position
+        result = list([2002, 'Ohio', 3.6, float('nan')])
+        self.assertTrue((result == frame.loc['three'].values).any())            #some casting needed
+
 
 if __name__ == '__main__':
     unittest.main()
