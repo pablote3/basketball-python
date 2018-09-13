1da30b0c7c821377e908848835be2c67eec8ed62
Paul Rossotti <rossotti.paul@gmail.com>
2018-09-13 06:09:42 -0700

    added read csv no header test

--- toolbox/pandas/Test_DataReader.py
+++ toolbox/pandas/Test_DataReader.py
@@ -13,7 +13,12 @@ class TestPandasDataReader(unittest.TestCase):
         result = pd.DataFrame([[1, 2, 3, 4, 'hello'], [5, 6, 7, 8, 'world'], [9, 10, 11, 12, 'foo']],
                               index=[0, 1, 2],
                               columns=['a', 'b', 'c', 'd', 'message'])
-        df = pd.read_csv(self.path + '/paulHeader.csv')
+        df = pd.read_csv(self.path + '/paulHeader.csv')                               #read csv with header
+        self.assertTrue(((result == df).all()).all())
+
+        result = pd.DataFrame([[1, 2, 3, 4, 'hello'], [5, 6, 7, 8, 'world'], [9, 10, 11, 12, 'foo']],
+                              index=[0, 1, 2])
+        df = pd.read_csv(self.path + '/paulNoHeader.csv', header=None)                #read csv without header
         self.assertTrue(((result == df).all()).all())
 
     def test_read_table(self):
@@ -24,31 +29,31 @@ class TestPandasDataReader(unittest.TestCase):
         self.assertTrue(((result == df).all()).all())
 
     # def test_read_web(self):
-        # import pandas_datareader.data as web
-        # all_data = {ticker: web.get_data_yahoo(ticker)
-        #             for ticker in ['AAPL', 'IBM', 'MSFT', 'GOOG']
-        #          }
-        # price = pd.DataFrame({ticker: data['Adj Close']
-        #                       for ticker, data in all_data.items()})
-        # volume = pd.DataFrame({ticker: data['Volume']
-        #                        for ticker, data in all_data.items()})
-        #
-        # returns = price.pct_change()
-        # series = pd.Series([0.001729, -0.004404, -0.012080, 0.000323], index=['AAPL', 'GOOG', 'IBM', 'MSFT'])
-        # self.assertFalse(((series == returns.iloc[1]).all()).all())
-        #
-        # self.assertEqual(0.4727037674161532, returns['MSFT'].corr(returns['IBM']))    #correlation between non-na values
-        # self.assertEqual(7.975231793694253e-05, returns['MSFT'].cov(returns['IBM']))  #covariance between non-na values
-        #
-        # series = pd.Series([0.429221, 1.000000, 0.394260, 0.508301], index=['AAPL', 'GOOG', 'IBM', 'MSFT'])
-        # self.assertFalse(((series == returns.corr().iloc[1]).all()).all())            #full correlation
-        # series = pd.Series([0.000103, 0.000230, 0.000071, 0.000109], index=['AAPL', 'GOOG', 'IBM', 'MSFT'])
-        # self.assertFalse(((series == returns.cov().iloc[1]).all()).all())             #full covariance
-        #
-        # series = pd.Series([0.361089, 0.394260, 1.000000, 0.472704], index=['AAPL', 'GOOG', 'IBM', 'MSFT'])
-        # self.assertFalse(((series == returns.corrwith(returns.IBM)).all()).all())     #correlation with column
-        # series = pd.Series([-0.066761, -0.017556, -0.162330, -0.087171], index=['AAPL', 'GOOG', 'IBM', 'MSFT'])
-        # self.assertFalse(((series == returns.corrwith(volume)).all()).all())          #correlation with value
+    #     import pandas_datareader.data as web
+    #     all_data = {ticker: web.get_data_yahoo(ticker)
+    #                 for ticker in ['AAPL', 'IBM', 'MSFT', 'GOOG']
+    #              }
+    #     price = pd.DataFrame({ticker: data['Adj Close']
+    #                           for ticker, data in all_data.items()})
+    #     volume = pd.DataFrame({ticker: data['Volume']
+    #                            for ticker, data in all_data.items()})
+    #
+    #     returns = price.pct_change()
+    #     series = pd.Series([0.001729, -0.004404, -0.012080, 0.000323], index=['AAPL', 'GOOG', 'IBM', 'MSFT'])
+    #     self.assertFalse(((series == returns.iloc[1]).all()).all())
+    #
+    #     self.assertEqual(0.4727037674161532, returns['MSFT'].corr(returns['IBM']))    #correlation between non-na values
+    #     self.assertEqual(7.975231793694253e-05, returns['MSFT'].cov(returns['IBM']))  #covariance between non-na values
+    #
+    #     series = pd.Series([0.429221, 1.000000, 0.394260, 0.508301], index=['AAPL', 'GOOG', 'IBM', 'MSFT'])
+    #     self.assertFalse(((series == returns.corr().iloc[1]).all()).all())            #full correlation
+    #     series = pd.Series([0.000103, 0.000230, 0.000071, 0.000109], index=['AAPL', 'GOOG', 'IBM', 'MSFT'])
+    #     self.assertFalse(((series == returns.cov().iloc[1]).all()).all())             #full covariance
+    #
+    #     series = pd.Series([0.361089, 0.394260, 1.000000, 0.472704], index=['AAPL', 'GOOG', 'IBM', 'MSFT'])
+    #     self.assertFalse(((series == returns.corrwith(returns.IBM)).all()).all())     #correlation with column
+    #     series = pd.Series([-0.066761, -0.017556, -0.162330, -0.087171], index=['AAPL', 'GOOG', 'IBM', 'MSFT'])
+    #     self.assertFalse(((series == returns.corrwith(volume)).all()).all())          #correlation with value
 
 
 if __name__ == '__main__':
